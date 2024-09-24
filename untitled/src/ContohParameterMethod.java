72a343028e3a6e022fc8b9cadb1f107d3e2d3ce1
Asafati Gea <2382006@unai.edu>
2024-09-24 10:59:54 +0700

    Tugas method

---
+++ untitled/src/ContohParameterMethod.java
@@ -0,0 +1,17 @@
+public class ContohParameterMethod {
+    public static int kaliDua(int angka){
+        int hasil = angka * 2;
+        return hasil;
+    }
+    public static double hitungLuasSegitiga(double alas, double tinggi){
+        return 0.5 * alas * tinggi;
+    }
+
+    public static void main(String[] args) {
+        int hasiKaliDua = kaliDua(angka:5);
+        system.out.println(*5 x 2 = * + hasilKalidua);
+
+        double luasSegitiga = hitungLuasSegitiga(alas: 4, tinggi: 6);
+        system.out.println("luas segitiga: " + luasSegitiga);
+    }
+}
